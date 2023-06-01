9dc61599af01e4c57f3d923448338bea0a0f68cb
defnath <nathaly.amao@tecsup.edu.pe>
2023-05-31 22:21:25 -0500

    Agregar archivos de proyecto.

---
+++ Xamarion09Gesture/Xamarion09Gesture/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Xamarion09Gesture
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Item1.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new TapDemo());
+            };
+            Item2.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new Pinch());
+            };
+            Item3.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new SwipeDemo());
+            };
+            Item4.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new PinchDemo());
+            };
+        }
+    }
+}
---
+++ Xamarion09Gesture/Xamarion09Gesture/TapDemo.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarion09Gesture
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+
+    public partial class TapDemo : ContentPage
+    {
+        bool isTap = false;
+
+        bool isVisible = false;
+        public TapDemo()
+        {
+            InitializeComponent();
+
+            var tap1 = new TapGestureRecognizer();
+            tap1.Tapped += EventTap;
+            lblTap1.GestureRecognizers.Add(tap1);
+
+            var tap2 = new TapGestureRecognizer();
+            tap2.Tapped += EventTapImage;
+            lblTap2.GestureRecognizers.Add(tap2);
+        }
+
+        void EventTap(object sender, EventArgs e)
+        {
+            isTap = !isTap;
+
+            if (isTap)
+                btnTap1.BackgroundColor = Color.Red;
+            else
+                btnTap1.BackgroundColor = Color.Green;
+        }
+
+        void EventTapImage(object sender, EventArgs e)
+        {
+            isVisible = !isVisible;
+
+            if (isVisible)
+                image1.Source = "tapped.png";
+            else
+                image1.Source = "tapped_bw.png";
+        }
+    }
+}
